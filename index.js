8e7d49ce3d0be8f3ac999a4b2076dd89d936e705
duo <106779045+4y6f@users.noreply.github.com>
2025-11-10 18:58:26 -0800

    add website

---
+++ index.js
@@ -0,0 +1,60 @@
+const pw = "access123";
+const overlay = document.getElementById('lock-overlay');
+const pwInput = document.getElementById('password');
+const msg = document.getElementById('msg');
+const unlockBtn = document.getElementById('unlockBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+const siteRoot = document.getElementById('site-root');
+
+const toggle = document.getElementById('theme-toggle');
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('light');
+});
+
+pwInput.focus();
+
+let tries = 0;
+const maxTries = 3;
+
+unlockBtn.addEventListener('click', checkPw);
+pwInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter') checkPw();
+});
+
+cancelBtn.addEventListener('click', () => {
+  document.documentElement.innerHTML = '';
+  window.location.href = 'https://google.com';
+});
+
+function checkPw() {
+  const input = pwInput.value.trim();
+  if (input === pw) {
+    unlock();
+  } else {
+    tries++;
+    msg.textContent = `Access Code invalid. (${maxTries - tries} attempts remaining.)`;
+    pwInput.value = '';
+    if (tries >= maxTries) {
+      msg.textContent = `You've reached the attempt limit. Redirecting.`;
+      setTimeout(() => {
+        document.documentElement.innerHTML = '';
+        window.location.href = 'https://google.com';
+      }, 1000);
+    }
+  }
+}
+
+function unlock() {
+  overlay.style.transition = 'opacity 0.6s ease';
+  overlay.style.opacity = '0';
+
+  setTimeout(() => {
+    overlay.style.display = 'none';
+    document.body.classList.remove('locked');
+    siteRoot.style.display = 'block';
+    setTimeout(() => {
+      siteRoot.style.opacity = '1';
+    }, 50);
+  }, 600);
+}
